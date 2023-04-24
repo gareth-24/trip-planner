c36a51a4fc9f18e39f908ec765e215b460e95371
Joe W <whobbes22@gmail.com>
2023-04-24 15:15:33 -0700

    add api call for seek keep via postal code

---
+++ src/js/event-service.js
@@ -0,0 +1,28 @@
+export default class EventService {
+  static async getEventService(lat,long){
+    try{
+      const response = await fetch(`https://api.seatgeek.com/2/events?geoip=94560&range=12mi&sort=score.desc&client_id=${process.env.API_KEY}`)
+      const jsoinifiedResponse = await response.json();
+
+      // MzMyNjYyMjV8MTY4MjM1OTcyNC42MzUxOTk - current api key
+
+      // fetch for lat and longitutde
+      // fetch(`https://api.seatgeek.com/2/events?lat=${lat}&lon=${long}&client_id=${process.env.API_KEY}`);
+
+      // fetch for ip address
+      // fetch(`https://api.seatgeek.com/2/events?geoip=true&client_id=${process.env.API_KEY}`);
+
+      //fetch for postal code and range and score
+      // fetch(`https://api.seatgeek.com/2/events?geoip=94560&range=12mi&sort=score.desc&client_id=${process.env.API_KEY}`);
+
+      // https://api.seatgeek.com/2/events?geoip=94560&range=12mi&sort=datetime_local.desc&sort=score.desc&client_id=MzMyNjYyMjV8MTY4MjM1OTcyNC42MzUxOTk (by postal,range,datetime, and score)
+      if(!response.ok){
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      return jsoinifiedResponse;
+    } catch(error){
+      return error;
+    }
+
+  }
+}
